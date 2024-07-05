51eb85406f59e294f3c0e3fddb0afbf300f7e1c4
Yaba Ernesto <yabaernesto@gmail.com>
2024-07-06 00:32:16 +0100

    fix: inicio do projeto

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+// arquivo de definicao de tipo
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
